d397498be71b0d1555a5c567cb0ea652434a3529
Zeynep Deniz <zeynep351961@gmail.com>
2023-04-04 18:08:30 +0300

    Creating Actions for colorReducer

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -7,13 +7,15 @@ import { colorReducer, initialState } from '../color-reducer';
 import hexColor from './hex-color';
 
 const Application = () => {
-  const [{hexcolor}, dispatch] = useReducer(colorReducer,initialState)
+  const [{ hexcolor }, dispatch] = useReducer(colorReducer,initialState)
 
   return (
     <div className="grid max-w-3xl grid-cols-1 gap-8 p-8 pb-40 mx-auto dark:bg-slate-900 dark:text-white sm:grid-cols-2">
       <ColorPicker
         hexColor={hexColor}
-        onChange={(e) => dispatch({ type: 'update-hex-color', payload: {hexColor: e.target.value} })}
+        onChange={(e) => dispatch({
+          type: 'update-hex-color', payload: { hexColor: e.target.value }
+        })}
       />
       <AdjustColors hexColor={hexColor} />
       <RelatedColors hexColor={hexColor} />
